e980f55831ad8f442d90393a230bedffc692f84a
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2014-06-17 08:21:17 +0200

    Add module docstring

--- __init__.py
+++ __init__.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 
+"""Transparent wrappers for pyfftw's NumPy FFT interfaces.
+
+Does nothing more but inserting a number of threads into your fft calls and
+handing that parameter to pyfftw.
+
+List of wrapped functions:
+fft, ifft, fft2, ifft2, fftn, ifftn, rfft, irfft, rfft2, irfft2, rfftn, irfftn,
+hfft, ihfft, hfft2, ihfft2, hfftn, ihfftn
+
+To save acquired wisdom, call save_wisdom(). Wisdom is automatically loaded on
+import.
+"""
+
+
 from transparent_pyfftw import *
